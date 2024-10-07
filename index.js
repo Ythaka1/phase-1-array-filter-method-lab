05dd143dbe1a9dd2badc0eeaaabb6130e71292d6
Hakim Waithaka <hakim.waithaka@gmail.com>
2024-10-07 14:20:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    return drivers.filter(driver => driver.toLowerCase === name.toLowerCase());
+    
+}
+const drivers = [`Bobby`, `Sammy`, `Bobby`, `Sally`]
+const name = "Bobby";
+
+console.log (findMatching(drivers, names); {
+    
+}
